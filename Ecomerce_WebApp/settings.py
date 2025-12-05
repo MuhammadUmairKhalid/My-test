9360c09a7a2c42c3b38158ee43083aba842e1245
muhammadumair <umairkhalid1314@gmail.com>
2025-12-05 16:32:11 +0500

    add the requirements

--- Ecomerce_WebApp/settings.py
+++ Ecomerce_WebApp/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d(di33z8%*yh9s%0w4m((7fi=ox5vn--f#@cki&=*7(zv+d0yy'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEGUB","FALSE").lower() == "true"
 AUTH_USER_MODEL = "Auth.User"
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
@@ -97,7 +97,10 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+import dj_database_url
+DATABASES['default'] = dj_database_url.parse(
+    os.environ.get("DATABASE_URL")
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
